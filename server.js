50e98405022d9ec5b252b7e17a636d20eb4ebf98
Pushkraj Savji <pushkarajsavji@gmail.com>
2022-10-28 17:42:59 +0530

    commit 2

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var http = require('http');
+const { response } = require("express");
+var express = require("express");
+var app = express();
+const path = require('path');
 
-http.createServer(function (req, res) {
-  res.write('Hello World!');
-  res.end(); 
-}).listen(5000); 
+app.get("/views/login.HTML", function (request, response) {
+  console.log("in login rouye");
+  response.sendFile("/views/login.HTML", { root: __dirname });
+});
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
