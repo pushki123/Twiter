cf74526aa032bc62b0ce45be01f44665ccbb6205
Pushkraj Savji <pushkarajsavji@gmail.com>
2022-10-31 23:18:42 +0530

    database

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require("express");
 var app = express();
 const PORT = 3000;
 
-app.get("/login", function (request, response) {
-  // console.log(request)
-  console.log("in login route");
-  response.sendFile("views/login.html", {root:__dirname});
+app.get("/login", function(request, response) {
+    // console.log(request)
+    console.log("in login route");
+    response.sendFile("views/login.html", { root: __dirname });
 });
 
-app.listen(PORT, function () {
-  console.log(`listening on port ${PORT}`);
+app.get("/user_info", function(request, response) {
+    // console.log(request)
+    console.log("in user info route");
+    response.sendFile("views/user_info.html", { root: __dirname });
+});
+
+app.listen(PORT, function() {
+    console.log(`listening on port ${PORT}`);
 });
